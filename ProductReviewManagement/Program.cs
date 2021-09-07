a65f8cd8424fd874eb9460978d3c3060d7e694cf
AkashSuchak <72850833+AkashSuchak@users.noreply.github.com>
2021-09-07 20:30:56 +0530

    Add project files.

---
+++ ProductReviewManagement/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ProductReviewManagement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To roduct review Management!");
+            Console.WriteLine("====================================");
+        }
+    }
+}
